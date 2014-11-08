596c5aa6b9735dc10da7231910e8ffdb7f463531
Joe Barrette <jbarrette21@gmail.com>
2014-11-07 20:44:45 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  })
+  .keydown(function(x) {  
+    //if x key ()
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  })
+  .keyup(function(x) {
+    //if x key ()
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+  });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
