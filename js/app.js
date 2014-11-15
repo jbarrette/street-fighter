55781a7b8ddc7c38d71eb55661d8f53ee8ba9197
Joe Barrette <jbarrette21@gmail.com>
2014-11-14 18:00:55 -0800

    third revision

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
+$('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
@@ -8,16 +7,16 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    playHadouken();
+    playHadouken();    
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show()
+    $('.hadouken').finish().show()
     .animate(
       {'left': '300px'},
       500,
       function() {
         $(this).hide();
-        $(this).css('left', '-190px');
+        $(this).css('left', '-212px');
       }
     );
   })
@@ -25,22 +24,19 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   })
-
-$(document).keydown(function(e) {
-    if(e.keyCode == 88) {
-      $('.ryu-still').hide();
-      $('.ryu-ready').hide();
-      $('.ryu-cool').show();
-    }
-  }).keyup(function(e) {
-    if(e.keyCode == 88) {
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-    }
+  .keydown(function(x) {  
+    //if x key ()
+    $('.ryu-ready').hide();
+    $('.ryu-cool').show();
+  })
+  .keyup(function(x) {
+    //if x key ()
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
   });
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play(); 
+  $('#hadouken-sound')[0].play();
 }
